a3faec88e365a4ce86f0a1cb387fc5a361e96420
Yue Zuo <woshizuoyue9201@yahoo.com>
2019-09-14 00:11:53 -0400

    dice image change

--- app.js
+++ app.js
@@ -9,11 +9,25 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
-dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    var diceDom = document.querySelector('.dice');
+
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+})
