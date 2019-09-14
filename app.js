1f51d3d096d4df65fbdd09b9af25964eaf484f45
Yue Zuo <woshizuoyue9201@yahoo.com>
2019-09-14 09:32:03 -0400

    update scoring

--- app.js
+++ app.js
@@ -30,4 +30,25 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
     diceDom.style.display = 'block';
     diceDom.src = 'dice-' + dice + '.png';
-})
+
+    if(dice !== 1){
+
+        roundScore += dice;
+    
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    }else{
+    
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    
+        roundScore = 0;
+    
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+    
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+    
+        document.querySelector('.dice').style.display = 'none';
+    }
+});
+
