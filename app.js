83c1218514a12c0f17b02fe2925836e2eecafe01
Yue Zuo <woshizuoyue9201@yahoo.com>
2019-09-15 12:02:05 -0400

    add new init

--- app.js
+++ app.js
@@ -11,16 +11,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
 
@@ -70,6 +61,8 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     }
 });
 
+document.querySelector('.btn-new').addEventListener('click', init);
+
 function nextPlayer(){
     
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
@@ -81,3 +74,21 @@ function nextPlayer(){
     document.querySelector('.dice').style.display = 'none';
 }
 
+function init(){
+
+    scores = [0,0];
+    activePlayer = 0;
+    roundScore = 0;
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+
+}
